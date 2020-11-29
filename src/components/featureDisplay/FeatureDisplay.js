b8fdd38370bba62ad8d41d904d15e1543403e9e0
NidhalAnis.Bouzara <nidhalanis.bouzara@gmail.com>
2020-11-29 19:56:44 +0100

    Made FeatureDisplay modular, made the featuers inside the wide Features container unique, added custom unique photos to the features.

--- src/components/featureDisplay/FeatureDisplay.js
+++ src/components/featureDisplay/FeatureDisplay.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-// Images
-import ingredients from '../../images/ingredients.jpg';
-
 // Styles
 import fdStyle from './FeatureDisplay.module.scss';
 
-const FeatureDisplay = () => {
+const FeatureDisplay = ({ image, title, description }) => {
     return (
         <div className={fdStyle.featureContainer}>
             <div className={fdStyle.imageContainer}>
-                <img src={ingredients} className={fdStyle.image} alt=""/>
+                <img src={image} className={fdStyle.image} alt=""/>
             </div>
-            <h3 className={fdStyle.title}>Ingredients</h3>
-            <p className={fdStyle.description}>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth.</p>
+            <h3 className={fdStyle.title}>
+                {title}
+            </h3>
+            <p className={fdStyle.description}>
+                {description}    
+            </p>
         </div>
     );
 }
--- src/containers/wide/Features/Features.js
+++ src/containers/wide/Features/Features.js
@@ -4,6 +4,11 @@ import FeatureDisplay from '../../../components/featureDisplay/FeatureDisplay';
 // Styles
 import featuresStyle from './Features.module.scss';
 
+// Images
+import ingredientsImage from '../../../images/ingredients-min.png';
+import nutritionImage from '../../../images/nutrition-min.png';
+import tasteImage from '../../../images/taste-min.png';
+
 const Features = () => {
     return (
         <section className={featuresStyle.container}>
@@ -11,9 +16,33 @@ const Features = () => {
                 <div className={featuresStyle.topDecoration}></div>
                 <h2 className={featuresStyle.subTitle}>Find the perfect dish for you</h2>
                 <div className={featuresStyle.featuresContainer}>
-                    <FeatureDisplay />
-                    <FeatureDisplay />
-                    <FeatureDisplay />
+                    <FeatureDisplay
+                        image={ingredientsImage}
+                        title={`
+                            Ingredients
+                        `}
+                        description={`
+                            But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth.
+                        `}
+                    />
+                    <FeatureDisplay
+                        image={nutritionImage}
+                        title={`
+                            Nutrition
+                        `}
+                        description={`
+                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise.
+                        `}
+                    />
+                    <FeatureDisplay
+                        image={tasteImage}
+                        title={`
+                            Taste
+                        `}
+                        description={`
+                            But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted.
+                        `}
+                    />
                 </div>
                 <div className={featuresStyle.bottomDecoration}></div>
             </div>
